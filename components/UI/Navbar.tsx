9a6a463ac8dd8fa5c6557383e66c6dc2674d1de7
David Alen <davidalen51@gmail.com>
2020-11-05 23:36:40 -0300

    [add] navbar

--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -4,8 +4,29 @@ import styles from "../../styles/components/Navbar.module.scss";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className={styles.Navbar}>
-      <h1 className={styles.title}>Rowse</h1>
+    <nav className={styles.navcontainer}>
+      <div className={styles.navbar}>
+        <h1 className={styles.flex2}>ROWSE</h1>
+        <div
+          className={`${styles.flex4} ${styles.center} ${styles.navbarcontent}`}
+        >
+          <a href="#">Shop</a>
+          <a href="#">Skin concerns</a>
+        </div>
+        <span className={`${styles.flex5} ${styles.center}`}>
+          The Greenhouse
+        </span>
+        <div
+          className={`${styles.flex4} ${styles.center} ${styles.navbarcontent}`}
+        >
+          <a href="#">Beauty Recipes</a>
+          <a href="#">Routines</a>
+        </div>
+        <div className={`${styles.flex2} ${styles.right}`}>
+          <a href="#">O</a>
+          <a href="#">D</a>
+        </div>
+      </div>
     </nav>
   );
 };
