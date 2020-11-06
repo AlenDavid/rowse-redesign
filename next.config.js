639a5f48536fdd0e5113fedd177d12700c6444b8
David Alen <davidalen51@gmail.com>
2020-11-05 22:25:51 -0300

    [add] fonts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 // next.config.js
 const withSass = require("@zeit/next-sass");
-module.exports = withSass({
-  cssModules: true,
-});
+const withFonts = require("nextjs-fonts");
+module.exports = withSass(
+  withFonts({
+    cssModules: true,
+  })
+);
