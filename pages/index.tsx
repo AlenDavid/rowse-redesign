46a8ea4daaa7d9948fc87a05f2b79bd97eb0a502
David Alen <davidalen51@gmail.com>
2020-11-07 20:04:02 -0300

    [add] header metas

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,19 @@ export default function Home() {
       <MetaTags>
         <title>Rowse</title>
         <meta name="description" content="Rowse's e-commerce" />
+        <meta name="og:title" content="Rowse's e-commerce" />
+        <meta name="og:owners" content="David Alen" />
+        <meta name="og:url" content="https://rowse-redesign.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@davidalen_" />
+        <meta
+          name="og:image"
+          content="https://rowse-redesign.vercel.app/images/hero-background-image.webp"
+        ></meta>
+        <meta
+          name="twitter:image"
+          content="https://rowse-redesign.vercel.app/images/hero-background-image.webp"
+        ></meta>
         <link rel="icon" href="/favicon.ico" />
       </MetaTags>
       <Container>
