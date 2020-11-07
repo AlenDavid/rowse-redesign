6e531a0ea1fa928432739f4279b29070584787ec
David Alen <davidalen51@gmail.com>
2020-11-07 18:46:44 -0300

    [add] small cards

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import styles from "../../styles/components/Hero.module.scss";
 import Navbar from "../UI/Navbar";
+import styles from "../../styles/components/Hero.module.scss";
 
 const Hero: React.FC = () => {
   return (
@@ -9,13 +9,31 @@ const Hero: React.FC = () => {
       <Navbar />
       <div className={styles.content}>
         <div className={styles.wrapper}>
-          <div className={styles.card}></div>
+          <div className={`${styles.card_big} ${styles.card}`}>
+            <span>Down</span>
+          </div>
         </div>
         <div className={styles.wrapper}>
-          <div className={styles.card_sm}></div>
-          <div className={styles.card_sm}></div>
-          <div className={styles.card_sm}></div>
-          <div className={styles.card_sm}></div>
+          <div className={`${styles.card_sm} ${styles.card}`}>
+            <h2>Care for mature skin</h2>
+            <p>Keep an eye out for selection of raw ingredients.</p>
+            <a href="#products">view products</a>
+          </div>
+          <div className={`${styles.card_sm} ${styles.card}`}>
+            <h2>Evening Primrose oil</h2>
+            <p>100% organic cold-pressed oil.</p>
+            <a href="#Evening-Primrose-oil">view product</a>
+          </div>
+          <div className={`${styles.card_sm} ${styles.card}`}>
+            <h2>Facial & Gua Sha kit</h2>
+            <p>Our all time best-seller back in stock</p>
+            <a href="#kit-facial-gua-sha">discover</a>
+          </div>
+          <div className={`${styles.card_sm} ${styles.card}`}>
+            <h2>Concerns & Routines</h2>
+            <p>Our all time best-seller back in stock</p>
+            <a href="#all-time-products">view products</a>
+          </div>
         </div>
       </div>
     </main>
